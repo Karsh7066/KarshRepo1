e04873821bdec0fbd6f7a2e7a209a4b10b93a6fa
Karsh7066 <shubhamgf7066@gmail.com>
2025-07-27 00:20:56 +0530

    Automatic commit using shortcut command

--- src/FlowControlDemo/IterativeWhileLoopDemo.java
+++ src/FlowControlDemo/IterativeWhileLoopDemo.java
@@ -1,5 +1,9 @@
 package FlowControlDemo;
 
 public class IterativeWhileLoopDemo {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Employee one is here");
+    }
 }
---
+++ src/GitSampleTest.java
@@ -0,0 +1,6 @@
+public class GitSampleTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello Git");
+    }
+}
